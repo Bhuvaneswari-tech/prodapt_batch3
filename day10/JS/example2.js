4f333115987300767243bf67fa49afa10fabf903
Bhuvaneswari-tech <bhuvanagitrepo@gmail.com>
2026-07-17 11:48:39 +0530

    ten commit

---
+++ day10/JS/example2.js
@@ -0,0 +1,22 @@
+//Primitive Data types
+let name = "Alice";       // String
+let age = 25;             // Number
+let isActive = true;      // Boolean
+let salary = null;        // Null
+let address;              // Undefined
+let id = Symbol("id");    // Symbol
+let big = 123456789n;     // BigInt
+
+//Non Primitive 
+let student = {
+    name: "John",
+    age: 22
+};
+
+let colors = ["Red", "Blue"];
+
+function greet(){
+    console.log("Hello")
+}
+
+greet()
